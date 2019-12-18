6d723d6954c7a8878e60d59883001d563e42f683
fawwazmayda <maydafawwaz@gmail.com>
2019-12-18 12:32:52 +0700

    Update Schemas.js

---
+++ Schemas.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://m001-student:<password>@sandbox-wtclz.mongodb.net/test?retryWrites=true&w=majority",
+{useNewUrlParser=true,dbName='ppl'})
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Sambung")
+});
+
+let fintechSchema = new mongoose.Schema({
+    id: String,
+    balance : Number
+})
+
+let fintech = mongoose.model('fintech',fintechSchema,'fintech')
+
+module.exports = fintech
