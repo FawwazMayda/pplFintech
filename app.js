50e77082ab53738608aeb02b0d84b128305c7c7e
fawwazmayda <maydafawwaz@gmail.com>
2019-12-18 12:34:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app =express()
 const cors = require('cors')
+const fintechController = require('./fintechController')
 
 app.use(cors())
+app.use('/fintech',fintechController)
 
 module.exports = app
