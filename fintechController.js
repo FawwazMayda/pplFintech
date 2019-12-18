5a877d7ed3efaa9b270621e823b48f6b9cbc4238
fawwazmayda <maydafawwaz@gmail.com>
2019-12-18 12:38:11 +0700

    create User

--- fintechController.js
+++ fintechController.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
+const fintech = require('./Schemas')
 router.use(bodyParser)
 
+router.get('/create/:id',(req,res)=>{
+    let userId = req.params.id 
+    fintech.find({id:userId},(err,d)=>{
+        if(err){
+            fintech.create({id:userId,balance:0})
+            res.status(200).send(`User created with id ${userId}`)
+        } else {
+            res.status(500).send({msg:"User already exits",data:d})
+        }
+    })
+
+})
+
 module.exports = router
+
