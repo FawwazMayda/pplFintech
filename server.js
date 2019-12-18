6e2a217d5c21523d40b0661742db19b7e5c5444d
fawwazmayda <maydafawwaz@gmail.com>
2019-12-18 12:26:18 +0700

    Update server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+//const app = require('./app')
+let port = 80
+let server = http.createServer(app)
+
+server.listen(port, ()=> console.log(`Listening at PORT ${port}`))
